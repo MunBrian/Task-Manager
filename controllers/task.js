f1463fca2b8f57361fa995708a26c72625c721cf
Brian Mungai <63002200+MunBrian@users.noreply.github.com>
2022-06-07 22:06:52 +0300

    Added next option in the controller function .

--- controllers/task.js
+++ controllers/task.js
@@ -24,7 +24,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
   res.status(200).json({ task });
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
 
@@ -36,7 +36,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
 
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
